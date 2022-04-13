4e8e7e21445e84f4417d01c24e534e9ea28eafd0
colet <cole.titze@mines.sdsmt.edu>
2022-04-12 21:41:53 -0600

    Working connection to azure, removed .idea files

--- GamePredictionTrigger/data_access.py
+++ GamePredictionTrigger/data_access.py
@@ -4,12 +4,12 @@ server = 'nhl-game.database.windows.net'
 database = 'Games'
 username = 'console'
 password = '{duvton-qofDic-1runxi}'
-driver = '{ODBC Driver 18 for SQL Server}'
+driver = '{ODBC Driver 17 for SQL Server}'
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password) as conn:
     with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 id FROM Games")
+        cursor.execute("SELECT TOP 3 id FROM Game")
         row = cursor.fetchone()
         while row:
-            print(str(row[0]) + " " + str(row[1]))
+            print(str(row[0]))
             row = cursor.fetchone()
