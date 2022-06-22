7d18db6b5cd2036af921d250be4d79f9f6692f37
Cole <cole.titze@outlook.com>
2022-06-22 15:41:34 -0500

    Split the test models function into its own file

--- GamePredictionTrigger/trigger.py
+++ GamePredictionTrigger/trigger.py
@@ -1,16 +1,19 @@
 import datetime
 import logging
 import MathModels.run_models as run_models
+import MathModels.test_models as test_models
 import azure.functions as func
+
+number_of_simulations = 5
 
 def start():
     accuracies = []
-    for i in range(10):
-        accuracy = run_models.test_models()
+    for i in range(number_of_simulations):
+        accuracy = test_models.test_models()
         accuracies.append(accuracy)
     print(max(accuracies))
 
-    run_models.predict_and_store_todays_games()
+    #run_models.predict_and_store_todays_games()
 
 
 def main(mytimer: func.TimerRequest) -> None:
