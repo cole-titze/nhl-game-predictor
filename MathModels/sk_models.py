9d3080c269e0a24643c3041a9fe3445ea0fea222
Cole Titze <cole.titze@outlook.com>
2022-07-10 11:04:09 -0500

    Try Guassian from desktop

--- MathModels/sk_models.py
+++ MathModels/sk_models.py
@@ -2,7 +2,9 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.ensemble import StackingClassifier
-
+from sklearn import svm
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
 
 class Model:
     def __init__(self, Name, Model):
@@ -13,29 +15,29 @@ class MathModels:
     def __init__(self):
         self.models = []
         knn = KNeighborsClassifier(n_neighbors=150)
-        mlp = MLPClassifier(solver='adam', max_iter=600, activation='relu', alpha=1e-5, hidden_layer_sizes=(8,))
-        rf = RandomForestClassifier(n_estimators=100, criterion='entropy')
-        mlp_ai_final = MLPClassifier(solver='adam', max_iter=600, activation='logistic', alpha=1e-5, hidden_layer_sizes=(8,))
+        mlp_test = MLPClassifier(solver='adam', max_iter=600, activation='relu', alpha=1e-5, hidden_layer_sizes=(32, 8))
+        rf = RandomForestClassifier(n_estimators=200, criterion='entropy')
+        mlp = MLPClassifier(solver='adam', max_iter=600, activation='logistic', alpha=1e-5, hidden_layer_sizes=(40,))
+        svm_clf = svm.SVC(probability=True)
+        kernel = 1.0 * RBF(1.0)
+        guass = GaussianProcessClassifier(kernel=kernel)
 
-        # Look into estimators_ with already fitted models (pick best models)
         ensemble = VotingClassifier(estimators=[
-            ("Random Forrest", rf), ("Best MLP Input Layer Match Attributes", mlp_ai_final)],
-            voting='soft')
+            ("MLP", mlp_test), ("Best MLP", mlp)], voting='soft')
 
         # Add weights based on log loss or cross-val
-        pca_ensemble_soft = VotingClassifier(estimators=[
-            ("Knn", knn), ("MLP", mlp), ("MLPFinal", mlp_ai_final)],
-            voting='soft')
+        voting_all = VotingClassifier(estimators=[
+            ("Knn", knn), ("MLP", mlp), ("rf", rf), ("svm", svm_clf), ("guass", guass)], voting='soft')
 
         pca_stacking = StackingClassifier(estimators=[
-            ("Knn", knn), ("MLP", mlp), ("MLPFinal", mlp_ai_final)])
+            ("Knn", knn), ("MLP", mlp), ("rf", rf), ("svm", svm_clf), ("guass", guass)])
 
-
-
-        self.models.append(Model("Knn", knn))
-        self.models.append(Model("MLP testing", mlp))
-        self.models.append(Model("Random Forrest", rf))
-        self.models.append(Model("Best MLP Input Layer Match Attributes", mlp_ai_final))
-        self.models.append(Model("Ensemble", ensemble))
-        self.models.append(Model("Ensemble PCA Soft", pca_ensemble_soft))
-        self.models.append(Model("Stacking PCA", pca_stacking))
+        #self.models.append(Model("KNN", knn))
+        #self.models.append(Model("SVM", svm_clf))
+        #self.models.append(Model("RF", rf))
+        #self.models.append(Model("MLP", mlp))
+        #self.models.append(Model("MLP Testing", mlp_test))
+        self.models.append(Model("Gaussian", guass))
+        #self.models.append(Model("Ensemble", ensemble))
+        #self.models.append(Model("Voting All", voting_all))
+        #self.models.append(Model("Stacking PCA", pca_stacking))
