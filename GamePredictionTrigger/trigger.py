f3e8f15765632f0f328461210f89681b8e1bbd97
Cole Titze <cole.titze@outlook.com>
2022-09-05 17:46:47 -0500

    Removed azure

--- GamePredictionTrigger/trigger.py
+++ GamePredictionTrigger/trigger.py
@@ -1,22 +1,10 @@
 import datetime
 import logging
 import MathModels.run_models as run_models
-import azure.functions as func
 
 def test():
     run_models.find_and_store_best_model()
 
 def start():
     run_models.predict_and_store_todays_games()
-
-
-def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
-    start()
     
