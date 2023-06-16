57d5ebc0166468ed896c07fbb4fddbe7945e2cc8
Cole Titze <cole.titze@outlook.com>
2023-06-15 20:02:37 -0500

    Normalizing all data and preventing testing data to leak

--- Entities/Mappers/train_test_mapper.py
+++ Entities/Mappers/train_test_mapper.py
@@ -33,7 +33,7 @@ def get_pca_train_test_data(game_list: np.array, year: int, chi_dimensions: int,
     x_train, y_train, x_test, y_test, predict_ids = get_train_test_data(game_list, year)
     if not x_test.any():
         return x_train, y_train, x_test, y_test, predict_ids
-    # standarize data
+    # standardize data
     sc = StandardScaler().fit(x_train)
     x_train = sc.transform(x_train)
     x_test = sc.transform(x_test)
@@ -43,12 +43,12 @@ def get_pca_train_test_data(game_list: np.array, year: int, chi_dimensions: int,
     x_test_norm = norm.transform(x_test)
     # Select features
     feature_selector = SelectKBest(chi2, k=chi_dimensions).fit(x_train_norm, y_train)
-    x_train = feature_selector.transform(x_train)
-    x_test = feature_selector.transform(x_test)
+    x_train_chi = feature_selector.transform(x_train_norm)
+    x_test_chi = feature_selector.transform(x_test_norm)
 
     # PCA input data (reduce dimensionality)
     pca = PCA(n_components=dimensions)
-    x_train = pca.fit_transform(x_train)
-    x_test = pca.transform(x_test)
+    x_train = pca.fit_transform(x_train_chi)
+    x_test = pca.transform(x_test_chi)
 
     return x_train, y_train, x_test, y_test, predict_ids
